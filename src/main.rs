37a1a9e8785eae07d66a49181afb9f39642f41d9
Nehliin <oskar.d.nehlin@gmail.com>
2020-08-14 23:57:45 +0200

    Try to complete TileMapPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,168 @@
+use bevy::{
+    asset::AssetPlugin,
+    diagnostic::DiagnosticsPlugin,
+    input::keyboard::KeyboardInput,
+    prelude::*,
+    render::{
+        camera::{Camera, OrthographicProjection, VisibleEntities},
+        pass::ClearColor,
+    },
+    scene::ScenePlugin,
+    sprite::SpritePlugin,
+    type_registry::{TypeRegistry, TypeRegistryPlugin},
+};
+use std::{fs::File, io::Write};
+use tilemap::{TileMap, TileMapLoader, TileMapPlugin, TileMapSpawner};
+#[derive(Properties, Default)]
+struct Player;
+#[derive(Properties, Default)]
+struct Enemy;
+#[derive(Properties, Default)]
+struct Health(u8);
+
+fn create_world(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut tile_maps: ResMut<TileMapSpawner>,
+) {
+    let enemy_size = Vec2::new(40.0, 30.0);
+    commands
+        .spawn(Camera2dComponents::default())
+        .spawn(UiCameraComponents::default())
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
+            translation: Translation(Vec3::new(55.0, 75.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(30.0, 50.0),
+            },
+            ..Default::default()
+        })
+        .with(Player)
+        .with(Health(100))
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(0.2, 1.0, 0.5).into()),
+            translation: Translation(Vec3::new(0.0, 5.0, 0.0)),
+            sprite: Sprite { size: enemy_size },
+            ..Default::default()
+        })
+        .with(Enemy)
+        .with(Health(100))
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(1.0, 0.2, 0.8).into()),
+            translation: Translation(Vec3::new(0.0, 0.0, 0.0)),
+            sprite: Sprite { size: enemy_size },
+            ..Default::default()
+        })
+        .with(Enemy)
+        .with(Health(100))
+        .spawn(SpriteComponents {
+            material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
+            translation: Translation(Vec3::new(25.0, 25.0, 0.0)),
+            sprite: Sprite { size: enemy_size },
+            ..Default::default()
+        })
+        .with(Enemy)
+        .with(Health(100));
+
+    let tilemap: Handle<TileMap> = asset_server.load("assets/scenes/test.json").unwrap();
+
+    tile_maps.spawn(tilemap);
+    //let map: Handle<TileMap> = asset_server.load("assets/scenes/test.json").unwrap();
+    println!("Done");
+}
+
+fn test_world(mut commands: Commands) {
+    commands
+        .spawn(Camera2dComponents::default())
+        .spawn((Player, Health(100)))
+        .spawn((Enemy, Health(100)));
+}
+
+fn load_world(asset_server: Res<AssetServer>, mut scene_spawner: ResMut<SceneSpawner>) {
+    let scene_handle: Handle<Scene> = asset_server.load("assets/scenes/start_scene.scn").unwrap();
+
+    scene_spawner.load(scene_handle);
+    println!("loaded: {:?}", asset_server.get_load_state(scene_handle));
+    asset_server.watch_for_changes().unwrap();
+}
+
+fn save_scene(world: &mut World, resources: &mut Resources) {
+    let type_registry = resources.get::<TypeRegistry>().unwrap();
+    let scene = Scene::from_world(&world, &type_registry.component.read().unwrap());
+
+    let mut file = File::create("assets/scenes/start_scene.scn").unwrap();
+    file.write_all(
+        scene
+            .serialize_ron(&type_registry.property.read().unwrap())
+            .unwrap()
+            .as_bytes(),
+    )
+    .unwrap();
+}
+#[derive(Default)]
+struct KeyboardEventReader {
+    event_reader: EventReader<KeyboardInput>,
+}
+
+fn input_system(
+    mut keyboard_event_reader: ResMut<KeyboardEventReader>,
+    keyboard_events: Res<Events<KeyboardInput>>,
+) {
+    for event in keyboard_event_reader.event_reader.iter(&keyboard_events) {
+        println!("Event: {:?}", event);
+    }
+}
+
+fn direct_input(world: &mut World, resources: &mut Resources) {
+    let mut should_save = false;
+    {
+        let key_input = resources.get::<Input<KeyCode>>().unwrap();
+        if key_input.pressed(KeyCode::LControl) && key_input.pressed(KeyCode::S) {
+            should_save = true;
+        }
+    }
+    if should_save {
+        println!("saved!");
+        save_scene(world, resources);
+    }
+}
+
+// This system prints all ComponentA components in our world. Try making a change to a ComponentA in load_scene_example.scn.
+// You should immediately see the changes appear in the console.
+fn print_system(mut query: Query<Entity>) {
+    for entity in &mut query.iter() {
+        println!("  Entity({})", entity.id());
+    }
+}
+
+fn create_camera(mut commands: Commands) {
+    commands.spawn(Camera2dComponents::default());
+    // .spawn(UiCameraComponents::default());
+}
+
+mod tilemap;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_default_plugins()
+        .add_plugin(TileMapPlugin)
+        //    .init_resource::<KeyboardEventReader>()
+        .register_component::<Health>()
+        .register_component::<Player>()
+        .register_component::<Enemy>()
+        .add_startup_system(create_world.system())
+        // .add_startup_system(load_world.system())
+        .add_system(direct_input.thread_local_system())
+        //.add_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
+        // .add_system(save_scene_system.thread_local_system())
+        //.add_startup_stage("start_stage")
+        //.add_stage("test")
+        //.add_stage_after("test", "load_stage")
+        //.add_startup_system_to_stage("start_stage", system_one.system())
+        //.add_system_to_stage("test", system_two.system())
+        //.add_system_to_stage("load_stage", system_three.system())
+        //.add_startup_system_to_stage("load_stage", system_two.system())
+        // .add_startup_system(save_scene_system.thread_local_system())
+        .run();
 }
---
+++ src/tilemap.rs
@@ -0,0 +1,204 @@
+use bevy::asset::{AssetLoader, HandleId, LoadState};
+use bevy::{prelude::*, sprite::TextureAtlasBuilder};
+use serde::Deserialize;
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
+
+#[derive(Debug, Default, Deserialize)]
+pub struct TileMap {
+    height: u32,
+    width: u32,
+    orientation: String,
+    layers: Vec<TileLayer>,
+    tilesets: Vec<TileSet>,
+    tilewidth: u32,
+    tileheight: u32,
+}
+#[derive(Debug, Default, Deserialize)]
+pub struct TileLayer {
+    data: Vec<u32>,
+    height: u32,
+    width: u32,
+    id: u32,
+    name: String,
+    opacity: f32,
+    x: u32,
+    y: u32,
+}
+#[derive(Debug, Default, Deserialize)]
+pub struct Grid {
+    height: u32,
+    width: u32,
+    orientation: String,
+}
+#[derive(Debug, Default, Deserialize)]
+pub struct TileSet {
+    columns: u32,
+    firstgid: i32,
+    grid: Grid,
+    margin: u32,
+    name: String,
+    spacing: u32,
+    tilecount: u32,
+    tileheight: u32,
+    tilewidth: u32,
+    tiles: Vec<Tile>,
+}
+#[derive(Debug, Default, Deserialize)]
+pub struct Tile {
+    id: u32,
+    image: String,
+    imageheight: u32,
+    imagewidth: u32,
+}
+#[derive(Default, Debug)]
+pub struct TileMapLoader;
+
+impl AssetLoader<TileMap> for TileMapLoader {
+    fn from_bytes(&self, _asset_path: &Path, bytes: Vec<u8>) -> Result<TileMap, anyhow::Error> {
+        let tile_map = serde_json::from_slice(&bytes)?;
+        Ok(tile_map)
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["json"]
+    }
+}
+
+#[derive(Default)]
+pub struct TileMapSpawner {
+    loaded_maps: HashMap<Handle<TileMap>, Handle<TextureAtlas>>,
+    tilemap_event_reader: EventReader<AssetEvent<TileMap>>,
+    staged_maps: HashMap<Handle<TileMap>, Vec<Handle<Texture>>>,
+    to_be_spawned: Vec<Handle<TileMap>>,
+}
+
+impl TileMapSpawner {
+    // show layer?
+    pub fn spawn(&mut self, handle: Handle<TileMap>) {
+        self.to_be_spawned.push(handle);
+    }
+
+    pub fn is_loaded(&self, handle: Handle<TileMap>) -> bool {
+        self.loaded_maps.contains_key(&handle)
+    }
+
+    fn stage_map(
+        &mut self,
+        handle: Handle<TileMap>,
+        tilemap_assets: &Assets<TileMap>,
+        asset_server: &AssetServer,
+    ) {
+        if self.staged_maps.contains_key(&handle) {
+            return;
+        }
+
+        let tilemap = tilemap_assets.get(&handle).unwrap();
+
+        let tiles = tilemap.tilesets.iter().flat_map(|set| &set.tiles);
+        let mut texture_handles = Vec::with_capacity(tiles.size_hint().0);
+
+        for tile in tiles {
+            let texture_handle: Handle<Texture> =
+                asset_server.load(PathBuf::from(&tile.image)).unwrap();
+            texture_handles.push(texture_handle);
+        }
+
+        self.staged_maps.insert(handle, texture_handles);
+    }
+
+    fn poll_staged_maps(
+        &mut self,
+        asset_server: &AssetServer,
+        texture_store: &mut Assets<Texture>,
+        texture_atlas_store: &mut Assets<TextureAtlas>,
+    ) {
+        for (tilemap_handle, staged_textures) in self.staged_maps.iter() {
+            let all_loaded = staged_textures.iter().any(|handle| {
+                if let LoadState::Loaded(_) = dbg!(asset_server.get_load_state(*handle).unwrap()) {
+                    true
+                } else {
+                    false
+                }
+            });
+
+            if all_loaded {
+                let mut texture_atlas_builder = TextureAtlasBuilder::default();
+                for texture_handle in staged_textures.iter() {
+                    let texture = texture_store.get(texture_handle).unwrap();
+                    texture_atlas_builder.add_texture(*texture_handle, &texture);
+                }
+                let texture_atlas = texture_atlas_builder.finish(texture_store).unwrap();
+                let atlas_handle = texture_atlas_store.add(texture_atlas);
+                self.loaded_maps.insert(*tilemap_handle, atlas_handle);
+            }
+        }
+    }
+}
+
+fn tilemap_load_system(
+    mut commands: Commands,
+    asset_events: Res<Events<AssetEvent<TileMap>>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut texture_atlas_store: ResMut<Assets<TextureAtlas>>,
+    mut tilemap_spawner: ResMut<TileMapSpawner>,
+    asset_server: Res<AssetServer>,
+    tilemap_store: Res<Assets<TileMap>>,
+    mut texture_store: ResMut<Assets<Texture>>,
+) {
+    for event in tilemap_spawner.tilemap_event_reader.iter(&asset_events) {
+        if let AssetEvent::Created { handle } | AssetEvent::Modified { handle } = event {
+            if let Some(tilemap) = tilemap_spawner.loaded_maps.get(handle) {
+                println!("Alredy Loaded {:?}", handle);
+            } else {
+                println!("Stageing map");
+                tilemap_spawner.stage_map(*handle, &tilemap_store, &asset_server);
+            }
+        }
+    }
+
+    tilemap_spawner.poll_staged_maps(&asset_server, &mut texture_store, &mut texture_atlas_store);
+
+    let loaded_maps = std::mem::replace(&mut tilemap_spawner.loaded_maps, HashMap::new());
+
+    let mut hack = Vec::new();
+    //rest of queue
+    tilemap_spawner
+        .to_be_spawned
+        .iter()
+        .filter(|&handle| {
+            if tilemap_spawner.is_loaded(*handle) {
+                println!("LOADED");
+                true
+            } else {
+                println!("NOT LOADED");
+                hack.push(*handle);
+                false
+            }
+        })
+        .map(|handle| loaded_maps.get(&handle).unwrap())
+        .for_each(|handle| {
+            let atlas = texture_atlas_store.get(handle).unwrap();
+            println!("spawned");
+            commands.spawn(SpriteComponents {
+                material: materials.add(atlas.texture.into()),
+                translation: Vec3::new(0.0, 0., 0.0).into(),
+                ..Default::default()
+            });
+        });
+    tilemap_spawner.to_be_spawned = hack;
+    tilemap_spawner.loaded_maps = loaded_maps;
+}
+
+pub struct TileMapPlugin;
+
+impl Plugin for TileMapPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_asset::<TileMap>()
+            .init_resource::<TileMapSpawner>()
+            .add_asset_loader::<TileMap, TileMapLoader>()
+            .add_system(tilemap_load_system.system());
+    }
+}
